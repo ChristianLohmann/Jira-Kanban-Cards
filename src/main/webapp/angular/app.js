7fc68c9b8dee8513eb572254976cbb26dbd3d134
Christian Lohmann <christianl.proxy@gmail.com>
2014-07-05 16:04:50 +0200

    Update app.js
    removed unnecessary code

--- src/main/webapp/angular/app.js
+++ src/main/webapp/angular/app.js
@@ -5,7 +5,6 @@
 var jiraKanbanCards = angular.module('jiraKanbanCards', ['ngResource', 'ngRoute', 'ngCookies', 'base64']);
 
 jiraKanbanCards.constant('cons', {
-    /* URL Suffix */
     POST: 'POST',
     GET: 'GET',
     PUT: 'PUT',
@@ -28,9 +27,5 @@ jiraKanbanCards
                     templateUrl: 'angular/cards/cardSearch.html',
                     controller: 'CardsController'
                 });
-            $httpProvider.defaults.useXDomain = true;
-            delete $httpProvider.defaults.headers.common['X-Requested-With'];
-//            jiraProvider.setUrl('https://techjira.zalando.net/rest/api/');
-//            jiraProvider.setUrlArray('/');
         }
     ]);
